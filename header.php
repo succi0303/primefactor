02abe819d9699b96075eb295bdbe194ad06b7792
Takashi Sugimoto <succi0303@gmail.com>
2012-02-26 22:50:45 +0900

    Open Graph Protocolの使用を明示した

--- header.php
+++ header.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
-<html>
+<html xmlns="http://ogp.me/ns#"
+xmlns:fb="http://www.facebook.com/2008/fbml">
 <head>
 <meta charset="<?php bloginfo('charset'); ?>" />
 <title><?php bloginfo('name'); ?><?php wp_title(); ?></title>
