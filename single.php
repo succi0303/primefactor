fd26fdb23abfc0519a525d5ed0339ddf0114426d
Takashi Sugimoto <succi0303@gmail.com>
2012-03-01 19:07:52 +0900

    disqusコメント投稿フォームを追加した

--- single.php
+++ single.php
@@ -27,6 +27,7 @@
 <?php if(have_posts()): while(have_posts()):
 the_post(); ?>
 <?php get_template_part('content','single'); ?>
+<?php comments_template(); ?>
 <?php endwhile; endif; ?>
 
 <p class="pagenation">
