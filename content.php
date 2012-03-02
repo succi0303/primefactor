aed174794a26cacce67172b8871afb3bf3e0bb07
Takashi Sugimoto <succi0303@gmail.com>
2012-03-02 14:49:12 +0900

    トップページにサムネイル画像を表示しないようにした

--- content.php
+++ content.php
@@ -2,8 +2,6 @@
 <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
 <p class="postcat"><?php the_category(' '); ?></p>
 
-<?php the_post_thumbnail(); ?>
-
 <?php the_content(); ?>
 
 <p class="postinfo">
