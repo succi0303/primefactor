d06df49be4b64a0047375a3d2b52cc8f58674f6a
すっちぃ <succi0303@gmail.com>
2012-04-07 14:52:00 +0900

    過去記事へのアクセスをページ番号を並べる形式に変更した。

--- pagenation.php
+++ pagenation.php
@@ -1,4 +1,29 @@
-<p class="pagenation">
-<span class="oldpage"><?php next_posts_link('&laquo; 古い記事'); ?></span>
-<span class="newpage"><?php previous_posts_link('新しい記事 &raquo;'); ?></span>
+<p class="pagenum">
+<?php $maxpage = $wp_query->max_num_pages;
+$current = $paged;
+if(!$current) {$current = 1;} ?>
+
+<?php $sidenum = 2;
+$shownum = $sidenum * 2 + 1;
+
+if($current > $sidenum &&
+$current < $maxpage-$sidenum+1){
+    $start = $current - $sidenum;
+    $end = $current + $sidenum;
+} elseif($current <= $sidenum){
+    $start = 1;
+    $end = min($shownum,$maxpage);
+} else {
+    $start = max($maxpage-$shownum+1,1);
+} ?>
+
+<?php previous_posts_link('&lt;'); ?>
+<?php for($i=$start; $i <= $end; $i++): ?>
+    <?php if($i == $current): ?>
+    <span><?php echo $i; ?></span>
+    <?php else: ?>
+    <a href="<?php echo get_pagenum_link($i); ?>"><?php echo $i; ?></a>
+    <?php endif; ?>
+<?php endfor; ?>
+<?php next_posts_link('&gt;'); ?>
 </p>
