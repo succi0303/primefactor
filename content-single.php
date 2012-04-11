2f09e918fe88b6c6bd087fd57154f73f74c40e16
すっちぃ <succi0303@gmail.com>
2012-04-12 01:07:34 +0900

    記事末尾の広告の位置を調整した

--- content-single.php
+++ content-single.php
@@ -6,13 +6,6 @@
 
 <?php wp_link_pages('before=<p id ="postpage">&after=</p>&pagelink=<span>%</span>'); ?>
 
-<p class="postinfo">
-<?php if(has_tag()): ?>
-<?php the_tags('','・'); ?> |
-<?php endif; ?>
-<?php echo get_the_date(); ?>
-</p>
-
 <div class="ad_under_post">
 <script type="text/javascript"><!--
 google_ad_client = "ca-pub-3774269786525137";
@@ -26,6 +19,13 @@ google_ad_height = 280;
 src="http://pagead2.googlesyndication.com/pagead/show_ads.js">
 </script>
 </div>
+
+<p class="postinfo">
+<?php if(has_tag()): ?>
+<?php the_tags('','・'); ?> |
+<?php endif; ?>
+<?php echo get_the_date(); ?>
+</p>
 
 <!-- X:S ZenBackWidget --><script type="text/javascript">document.write(unescape("%3Cscript")+" src='http://widget.zenback.jp/?base_uri=http%3A//succi.jp/blog/&nsid=103913574241442044%3A%3A103913595716275035&rand="+Math.ceil((new Date()*1)*Math.random())+"' type='text/javascript'"+unescape("%3E%3C/script%3E"));</script><!-- X:E ZenBackWidget -->
 
