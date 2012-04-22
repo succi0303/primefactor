8e1fb3801fc092e1cd60db30e4bc1bb55baf38db
すっちぃ <succi0303@gmail.com>
2012-04-22 17:43:56 +0900

    記事本文の広告位置を調整した(タイトル末尾にリンクユニットを追加し、記事末尾のレクタングル大を右詰めにした)

--- content-single.php
+++ content-single.php
@@ -2,6 +2,20 @@
 <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
 <p class="postcat"><?php the_category(' '); ?></p>
 
+<div class="ad_under_the_title">
+<script type="text/javascript"><!--
+google_ad_client = "ca-pub-3774269786525137";
+/* PF-リンク468x15 */
+google_ad_slot = "5295017566";
+google_ad_width = 468;
+google_ad_height = 15;
+//-->
+</script>
+<script type="text/javascript"
+src="http://pagead2.googlesyndication.com/pagead/show_ads.js">
+</script>
+</div>
+
 <?php the_content(); ?>
 
 <?php wp_link_pages('before=<p id ="postpage">&after=</p>&pagelink=<span>%</span>'); ?>
@@ -17,8 +31,7 @@ google_ad_height = 280;
 </script>
 <script type="text/javascript"
 src="http://pagead2.googlesyndication.com/pagead/show_ads.js">
-</script>
-</div>
+</script></div>
 
 <p class="postinfo">
 <?php if(has_tag()): ?>
